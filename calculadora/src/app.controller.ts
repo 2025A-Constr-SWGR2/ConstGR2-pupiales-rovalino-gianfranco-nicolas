be81076c4f204b65e1c0bff305a0c18e690078bf
Gianfranco Nicolás <gianfrancopr08@gmail.com>
2025-06-22 19:25:04 -0500

    feat(Gian P): Taller 4

--- calculadora/src/app.controller.ts
+++ calculadora/src/app.controller.ts
@@ -1,12 +1,29 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('database')
+  getDatabaseConnection() {
+    return {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      DB_HOST: this.configService.get('DB_HOST'),
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      DB_PASSWORD: this.configService.get('DB_PASSWORD'),
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      DB_NAME: this.configService.get('DB_NAME'),
+      DB_USER: process.env.DB_USER,
+    };
   }
 }
