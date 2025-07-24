b9c550ad9abca028d86cc6fdbe112b4a523c4d20
Gianfranco Nicolás <gianfrancopr08@gmail.com>
2025-07-23 22:48:22 -0500

    Add docker-compose.yml

---
+++ mi-libreria-matematica/testLib.js
@@ -0,0 +1,6 @@
+const math = require('./mathLib');
+
+console.log('suma(2,1) =', math.suma(2, 1));             // 3
+console.log('resta(3,2) =', math.resta(3, 2));           // 1
+console.log('multiplicacion(2,5) =', math.multiplicacion(2, 5)); // 10
+console.log('division(9,3) =', math.division(9, 3));     // 3
